2d031f88ebacdf554405141812fb52b4b74e735e
Macieira Maciel <macielgm@8c85901e4a59.ant.amazon.com>
2019-09-20 14:28:19 +0200

    All Files

---
+++ EBS.py
@@ -0,0 +1,28 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+sns = boto3.client('sns')
+
+response = ec2.describe_volume_status(
+    Filters=[
+        {
+            'Name': 'volume-status.status',
+            'Values': [
+                'impaired',
+            ]
+        },
+    ]
+)
+
+VolumeIds=[]
+for volume in response['VolumeStatuses']:
+    VolumeIds.append(volume['VolumeId'])
+VolumeIds_string = ','.join(VolumeIds)
+
+if VolumeIds_string:
+    response = sns.publish(
+        TopicArn='arn:aws:sns:eu-west-1:777996869152:Teste',
+        Message=" The following volumes are impaired: " + VolumeIds_string,
+    )
+
+print(VolumeIds_string)
---
+++ EBS_Lambda.py
@@ -0,0 +1,37 @@
+import boto3
+
+from botocore.exceptions import ClientError
+
+ec2 = boto3.client('ec2')
+sns = boto3.client('sns')
+
+
+def lambda_handler(event, context):
+
+    try:
+        response = ec2.describe_volume_status(
+            Filters=[
+                {
+                    'Name': 'volume-status.status',
+                    'Values': [
+                        'ok',
+                    ]
+                },
+            ]
+        )
+    except ClientError as e:
+        print("Some error has occurred! ", str(e))
+        exit(-1)
+    else:
+        VolumeIds=[]
+        for volume in response['VolumeStatuses']:
+            VolumeIds.append(volume['VolumeId'])
+        VolumeIds_string = ','.join(VolumeIds)
+
+        if VolumeIds_string:
+            response = sns.publish(
+                TopicArn='arn:aws:sns:eu-west-1:777996869152:Teste',
+                Message=" The following volumes are impaired: " + VolumeIds_string,
+            )
+
+        print(VolumeIds_string)
